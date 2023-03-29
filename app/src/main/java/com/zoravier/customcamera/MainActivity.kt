cf1d87ed8e04fc4e757a838797c8f80ebbebb878
zoravi1998 <anujk270@gmail.com>
2023-03-30 00:48:19 +0530

    theme update

--- app/src/main/java/com/zoravier/customcamera/MainActivity.kt
+++ app/src/main/java/com/zoravier/customcamera/MainActivity.kt
@@ -2,10 +2,17 @@ package com.zoravier.customcamera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.zoravier.customcamera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
